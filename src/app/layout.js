b896afd210dfe6f6914650c57bca2e78ba339782
Jayro Gómez <jaxjayro@gmail.com>
2023-02-12 13:24:32 -0600

    Added About folder dir, and integrata a Navbar on the Layout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,34 @@
-export default function RootLayout({ children }) {
+import Link from 'next/link'
+
+const links = [{
+  label: 'Home',
+  route: '/'
+}, {
+  label: 'About',
+  route: '/about'
+}]
+
+export default function RootLayout ({ children }) {
   return (
     <html>
-      <head />
-      <body>{children}</body>
+      <body>
+        <header>
+          <nav>
+            <ul>
+              {links.map(({ label, route }) => {
+                return (
+                  <li key={route}>
+                    <Link href={route}>
+                      {label}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   )
 }
